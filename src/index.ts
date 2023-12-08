39dde741b553ad1aa1481f2b16564fe1a61e9d6b
wapa5pow <wapa5pow@gmail.com>
2023-12-08 15:58:08 +0900

    index.tsを整理

--- src/index.ts
+++ src/index.ts
@@ -4,45 +4,49 @@ import { readSettingFile } from "./schema/setting";
 import { readTable } from "./table/table";
 import { Validator } from "./validator/validator";
 
-function validateArgs(args: string[]): Result<void, string> {
-	const usage = "Usage: csvv schema.yaml data.csv";
-	if (args.length !== 4) {
-		return Result.failure(usage);
+if (require.main === module) {
+	function validateArgs(args: string[]): Result<void, string> {
+		const usage = "Usage: csvv schema.yaml data.csv";
+		if (args.length !== 4) {
+			return Result.failure(usage);
+		}
+		if (!args[2].endsWith(".yaml")) {
+			return Result.failure(usage);
+		}
+		if (!args[3].endsWith(".csv")) {
+			return Result.failure(usage);
+		}
+		return Result.success(undefined);
 	}
-	if (!args[2].endsWith(".yaml")) {
-		return Result.failure(usage);
-	}
-	if (!args[3].endsWith(".csv")) {
-		return Result.failure(usage);
-	}
-	return Result.success(undefined);
-}
 
-async function main() {
-	{
-		const result = validateArgs(process.argv);
-		if (result.isError()) {
-			console.error(result.error);
+	async function main() {
+		{
+			const result = validateArgs(process.argv);
+			if (result.isError()) {
+				console.error(result.error);
+				process.exit(1);
+			}
+		}
+
+		// yaml読み込み
+		const schemaPath = process.argv[2];
+		const schema = readSettingFile(schemaPath);
+
+		// csv読み込み
+		const table = readTable(process.argv[3]);
+		const validator = new Validator();
+		const result = validator.validate(table, schema);
+		if (result.error === undefined) {
+			console.log("エラーなし");
+		} else {
+			for (const error of result.error) {
+				console.error(error.message);
+			}
 			process.exit(1);
 		}
 	}
 
-	// yaml読み込み
-	const schemaPath = process.argv[2];
-	const schema = readSettingFile(schemaPath);
-
-	// csv読み込み
-	const table = readTable(process.argv[3]);
-	const validator = new Validator();
-	const result = validator.validate(table, schema);
-	if (result.error === undefined) {
-		console.log("エラーなし");
-	} else {
-		for (const error of result.error) {
-			console.error(error.message);
-		}
-		process.exit(1);
-	}
+	main();
 }
 
-main();
+export { Validator };
