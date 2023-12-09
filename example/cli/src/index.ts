b9b1b70400838b2db6a9843d3b2273562c7c9053
wapa5pow <wapa5pow@gmail.com>
2023-12-09 15:29:13 +0900

    add example cli

---
+++ example/cli/src/index.ts
@@ -0,0 +1,35 @@
+import { Validator, convertToSchema, convertToTable } from "csvv";
+import fs from "fs";
+
+function validateArgs(args: string[]) {
+	const usage = "Usage: csvv schema.yaml data.csv";
+	if (args.length !== 4) {
+		throw Error(usage);
+	}
+	if (!args[2].endsWith(".yaml")) {
+		throw Error(usage);
+	}
+	if (!args[3].endsWith(".csv")) {
+		throw Error(usage);
+	}
+}
+async function main() {
+	validateArgs(process.argv);
+
+	// yaml読み込み
+	const schema = convertToSchema(fs.readFileSync(process.argv[2], "utf8"));
+	// csv読み込み
+	const table = convertToTable(fs.readFileSync(process.argv[3], "utf8"));
+
+	const validator = new Validator();
+	const errors = validator.validate(table, schema);
+	if (errors.length === 0) {
+		console.log("エラーなし");
+	} else {
+		for (const error of errors) {
+			console.error(error.message);
+		}
+		process.exit(1);
+	}
+}
+main();
